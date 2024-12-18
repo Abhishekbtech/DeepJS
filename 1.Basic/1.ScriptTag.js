5778729356d9c1ac1113b0263c4a562ebc5464fb
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-12-19 00:49:45 +0530

    first commit

---
+++ 1.Basic/1.ScriptTag.js
@@ -0,0 +1,32 @@
+
+// The <script> Tag
+
+// In HTML, JavaScript code is inserted between <script> and </script> tags.
+
+// new 
+
+// <script>
+//     document.getElementById("demo").innerHTML = "My First JavaScript";
+// </script>
+
+// old
+
+// Old JavaScript examples may use a type attribute: <script type="text/javascript">.
+// The type attribute is not required. JavaScript is the default scripting language in HTML.
+
+
+// Scripts can be placed in the <body>, or in the <head> section of an HTML page, or in both.
+// but
+// Placing scripts at the bottom of the <body> element improves the display speed, because script interpretation slows down the display.
+
+
+// To use an external script, put the name of the script file in the src (source) attribute of a <script> tag
+
+// <script src="script.js"></script>
+
+// External References
+// An external script can be referenced in 3 different ways:
+
+// With a full URL (a full web address)
+// With a file path (like /js/)
+// Without any path
