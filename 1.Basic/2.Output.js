2551f507455ca748c2d4ef85719ced038ca85713
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-12-19 11:31:13 +0530

    output method

---
+++ 1.Basic/2.Output.js
@@ -0,0 +1,41 @@
+
+// JavaScript Display Possibilities
+
+// Writing into an HTML element, using innerHTML.
+// Writing into the HTML output using document.write().
+// Writing into an alert box, using window.alert().
+// Writing into the browser console, using console.log().
+
+
+{/* <script>
+    document.getElementById("demo").innerHTML = 5 + 6;
+</script> */}
+
+// Changing the innerHTML property of an HTML element is a common way to display data in HTML.
+
+// <script>
+//     document.write(5 + 6);
+// </script>
+
+// The document.write() method should only be used for testing.
+// Using document.write() after an HTML document is loaded, will delete all existing HTML
+
+{/* <script>
+    window.alert(5 + 6);
+</script> */}
+
+// In JavaScript, the window object is the global scope object. This means that variables, properties, and methods by default belong to the window object. This also means that specifying the window keyword is optional
+
+{/* <script>
+    console.log(5 + 6);
+</script> */}
+
+// For debugging purposes, you can call the console.log() method in the browser to display data.
+
+
+
+// JavaScript Print
+
+// <button onclick="window.print()">Print this page</button>
+
+// The only exception is that you can call the window.print() method in the browser to print the content of the current window.
