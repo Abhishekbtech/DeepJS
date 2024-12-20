f9774821a3ec25ef87ffc9a4830f6a2d9eb4ef17
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-12-20 10:53:37 +0530

    add more methods

--- String/Methods.js
+++ String/Methods.js
@@ -43,3 +43,59 @@ console.log("substring", str.substring(1, 6));
 // substr() :-  substr() is similar to slice(). The difference is that the second parameter specifies the length of the extracted part.
 // str.substr(start [, length])
 console.log("substr", str.substr(1, 6));
+
+
+
+// NO NEED TO REMBER JUST FOR KNOWLEDGE
+
+
+// toUpperCase() :- A string is converted to upper case with toUpperCase():
+console.log("toUpperCase", str.toUpperCase());
+
+
+// toLowerCase() :- A string is converted to lower case with toLowerCase():
+console.log("toLowerCase", str.toLowerCase());
+
+
+// concat() :-  joins two or more strings: we can use +
+var x = "Hello ";
+var y = "World!";
+console.log("concat", x.concat(y));
+
+
+// trim() :- method removes whitespace from both sides of a string:
+let text1 = "      Hello World!      ";
+console.log("trim", text1.trim());
+
+
+// trimStart() :- The trimStart() method works like trim(), but removes whitespace only from the start of a string.
+console.log("trimStart", text1.trimStart());
+
+
+// trimEnd() :- The trimEnd() method works like trim(), but removes whitespace only from the end of a string.
+console.log("trimEnd", text1.trimEnd());
+
+
+// padStart() :- The padStart() method pads a string from the start. It pads a string with another string (multiple times) until it reaches a given length.
+console.log("padStart", 'abc'.padStart(8, '0'));
+
+
+// padEnd() :- method pads a string from the end.
+console.log("padEnd", 'abc'.padEnd(4, 'x'));
+
+
+// repeat() :- method returns a string with a number of copies of a string.
+console.log('repeat', 'hello'.repeat(3));
+
+
+// replace() :- method replaces a specified value with another value in a string, method replaces only the first match
+console.log("replace", str.replace("Abhishek", "Abhi"));
+
+
+// ReplaceAll() :- method allows you to specify a regular expression instead of a string to be replaced
+let str1 = "Abhishek kya hal Abhishek or kya ho Abhishek "
+console.log("replaceAll", str.replaceAll("Abhishek", "Abhi"));
+
+
+// split() :- A string can be converted to an array with the split() method
+console.log("split", str.split(","));
