f0c12263876a4f68b8dbdbdf3b1fbf05649261f6
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-12-20 12:46:56 +0530

    add basic array

---
+++ Array/Basic.js
@@ -0,0 +1,10 @@
+
+// JavaScript Arrays
+
+// An array is a special variable, which can hold more than one value
+const cars = ["Saab", "Volvo", "BMW"];
+let car = cars[0];                              // an array element by referring to the index number
+cars[0] = "Opel";                               // This statement changes the value of the first element in cars
+console.log(cars);                              
+
+console.log(car.toString())                     // onverts an array to a string of (comma separated) array values
