ca3e1fc75d4af20f06ef0f6f4fc9bb9de311aa05
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-12-22 16:06:39 +0530

    add scope

--- Basic/Scope.js
+++ Basic/Scope.js
@@ -15,21 +15,49 @@
 // Variables declared inside a { } block cannot be accessed from outside the block
 
 {
-    let x = 2;
-  }
-  // x can NOT be used here
+  let x = 2;
+}
+// x can NOT be used here
 
 
 // Local Scope
 // Variables declared within a JavaScript function, are LOCAL to the function
 
 function myFunction() {
-    let carName = "Volvo";
-  }
-  
-  // code here can NOT use carName
+  let carName = "Volvo";
+}
 
-  // Local variables have Function Scope
+// code here can NOT use carName
+// Local variables have Function Scope
 
 
-  
+// Function Scope
+
+// JavaScript has function scope: Each function creates a new scope.
+// Variables defined inside a function are not accessible (visible) from outside the function.
+// Variables declared with var, let and const are quite similar when declared inside a function.
+
+function myFunction() {
+  var carName = "Volvo";   // Function Scope
+}
+
+
+// Global Scope
+
+// Variables declared Globally (outside any function) have Global Scope.
+// Global variables can be accessed from anywhere in a JavaScript program.
+// Variables declared with var, let and const are quite similar when declared outside a block
+
+
+// Automatically Global
+
+// If you assign a value to a variable that has not been declared, it will automatically become a GLOBAL variable.
+// This code example will declare a global variable carName, even if the value is assigned inside a function
+
+myFunction();
+
+function myFunction() {
+  carName = "Volvo";
+}
+
+console.log("c", carName)
