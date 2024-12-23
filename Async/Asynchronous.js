c96571187c07be14dab990a6008013eecc4511dc
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-12-23 15:41:32 +0530

    add

---
+++ Async/Asynchronous.js
@@ -0,0 +1,14 @@
+
+// Asynchronous JavaScript
+
+// Functions running in parallel with other functions are called asynchronous
+
+
+// Waiting for a Timeout
+
+// When using the JavaScript function setTimeout(), you can specify a callback function to be executed on time-out
+
+
+// Waiting for Intervals
+
+//  When using the JavaScript function setInterval(), you can specify a callback function to be executed for each interval:
