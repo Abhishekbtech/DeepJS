b384e3689db50a2acfdbee97eac0bd28fb06e3d1
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-12-29 11:06:47 +0530

    add JSON data type

---
+++ JSON/DataType.js
@@ -0,0 +1,19 @@
+
+// JSON Data Types
+
+
+// Valid Data Types
+
+// a string
+// a number
+// an object (JSON object)
+// an array
+// a boolean
+// null
+
+
+// JSON values cannot be one of the following data types:
+
+// a function
+// a date
+// undefined
