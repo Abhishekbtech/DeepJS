ec2243bc1a3a81e2cce4fb2633904d39b6c42392
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-12-29 12:38:37 +0530

    add css

---
+++ JQuery/CSS.js
@@ -0,0 +1,29 @@
+
+// JavaScript / jQuery CSS Styles
+
+
+// Hiding HTML Elements
+
+//jQuery
+myElement.hide();
+
+// js
+myElement.style.display = "none";
+
+
+// Showing HTML Elements
+
+// jQuery
+myElement.show();
+
+// js
+myElement.style.display = "";
+
+
+// Styling HTML Elements
+
+// jQuery
+$("#demo").css("font-size","35px");
+
+// Js
+document.getElementById("demo").style.fontSize = "35px";
