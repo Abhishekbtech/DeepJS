0d975cdc5c9bad69ceede3c43101beafe2e622bb
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-12-29 12:45:50 +0530

    add dom

---
+++ JQuery/DOM.js
@@ -0,0 +1,19 @@
+
+// JavaScript / jQuery HTML DOM
+
+// Removing HTML Elements
+
+// jQury
+$("#id02").remove();
+
+// js
+document.getElementById("id02").remove();
+
+
+// Get Parent Element
+
+// JQuery
+myParent = $("#02").parent().prop("nodeName"); ;
+
+// js
+myParent = document.getElementById("02").parentNode.nodeName;
