e061a0ad3528cb604d9cac23cd5a4dac099d3cf5
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-12-29 11:59:20 +0530

    add

---
+++ JSON/JSON.js
@@ -0,0 +1,16 @@
+
+// JSONP (JSONP stands for JSON with Padding)
+
+// JSONP is a method for sending JSON data without worrying about cross-domain issues.
+// JSONP does not use the XMLHttpRequest object.
+// JSONP uses the <script> tag instead.
+
+
+// JSONP Intro
+
+
+// Requesting a file from another domain can cause problems, due to cross-domain policy.
+// Requesting an external script from another domain does not have this problem.
+// JSONP uses this advantage, and request files using the script tag instead of the XMLHttpRequest object.
+
+<script src="demo_jsonp.php"></script>
