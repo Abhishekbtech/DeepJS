8ddb68cb9ada689edfcd083ff7cdd2dd70594a25
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-12-29 11:23:59 +0530

    add example

--- JSON/Parse.js
+++ JSON/Parse.js
@@ -4,3 +4,6 @@
 // A common use of JSON is to exchange data to/from a web server.
 // When receiving data from a web server, the data is always a string.
 // Parse the data with JSON.parse(), and the data becomes a JavaScript object.
+
+const st = '{"name":"John", "age":30, "city":"New York"}'
+const obj = JSON.parse(st);
